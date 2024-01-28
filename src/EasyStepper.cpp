e260e393a570bb296d3f508791ecb4b3f4aedaaa
jcj4vier <157937537+jcj4vier@users.noreply.github.com>
2024-01-28 11:49:31 +0800

    Add files via upload

--- src/EasyStepper.cpp
+++ src/EasyStepper.cpp
@@ -1,1 +1,36 @@
+#include "EasyStepper.h"
 
+EasyStepper::EasyStepper(byte pulPin, byte dirPin, byte limitSwitchPin, int direction)
+  : stepper(AccelStepper::DRIVER, pulPin, dirPin) {
+  this->limitSwitchPin = limitSwitchPin;
+  stepper.setPinsInverted(false, false, true); // Invert the direction and enable the driver
+  stepper.setAcceleration(0); // Default acceleration value, can be adjusted
+  stepper.setMaxSpeed(1000.0); // Default max speed value, can be adjusted
+  stepper.setCurrentPosition(0);
+  pinMode(limitSwitchPin, INPUT_PULLUP); // Assuming the limit switch is connected to ground when pressed
+}
+
+void EasyStepper::easySetup(int maxSpeed, int acceleration, int maxSteps) {
+  stepper.setMaxSpeed(maxSpeed);
+  stepper.setAcceleration(acceleration);
+  stepper.setCurrentPosition(0);
+}
+
+void EasyStepper::returnToHome(int maxSteps) {
+  if (digitalRead(limitSwitchPin) == LOW) {
+    stepper.moveTo(maxSteps);
+    while (digitalRead(limitSwitchPin) == LOW) {
+      stepper.run();
+      delay(1);
+    }
+  }
+  stepper.setCurrentPosition(0);
+}
+
+void EasyStepper::stepperMove(int steps) {
+  stepper.moveTo(steps);
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+    delay(1);
+  }
+}
--- src/EasyStepper.h
+++ src/EasyStepper.h
@@ -1,1 +1,25 @@
+#ifndef EASYSTEPPER_H
+#define EASYSTEPPER_H
 
+#include <AccelStepper.h>
+
+class EasyStepper {
+public:
+  // Constructor
+  EasyStepper(byte pulPin, byte dirPin, byte limitSwitchPin, int direction = 1);
+
+  // Function to set up the stepper motor
+  void easySetup(int maxSpeed, int acceleration, int maxSteps);
+
+  // Function to return the stepper motor to the home position using a limit switch
+  void returnToHome(int maxSteps);
+
+  // Function to move the stepper motor to a specified number of steps
+  void stepperMove(int steps);
+
+private:
+  AccelStepper stepper;
+  byte limitSwitchPin;
+};
+
+#endif
